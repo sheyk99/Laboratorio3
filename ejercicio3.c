5ffa787773ad192f372554ff00c7202dbff15258
sheyk99 <shey.millersegura@gmail.com>
2024-05-07 20:08:41 -0600

    Comentarios de la implementacion

--- ejercicio3.c
+++ ejercicio3.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#define SIZE 5
+#include <stdlib.h> //libreria para rand
+#include <time.h> //libreria para rand
+#define SIZE 5 //Establece el macro para el tamano predeterminado
 
 
 int crearmatriz(int tama) {
 
     int matrix[tama][tama], i, j;
-    srand(time(NULL));
+    srand(time(NULL)); //establece rand con respecto al tiempo en el que se ejecuta el programa
+    //asi los valores no se repiten
 
-    if (tama <= 10) {
+    if (tama <= 10) { //delimita el mayor tamano posible de la matriz
         printf("La matriz utilizada corresponde a:\n");
-        for (i = 0; i < tama; i++) {
-            for (j = 0; j < tama; j++) {
-                matrix[i][j] = rand() % 2;
-                printf("%d", matrix[i][j]);
+        for (i = 0; i < tama; i++) { //recorre las filas de la matriz
+            for (j = 0; j < tama; j++) { //recorre las columnas de la matriz
+                matrix[i][j] = rand() % 2; //asigna a cada posicion un numero aleatorio menor a 2 (0 y 1)
+                printf("%d", matrix[i][j]); 
         }
         printf("\n");
         
@@ -35,30 +36,21 @@ int crearmatriz(int tama) {
 
 }
 
-int contarmatriz(int matrix, int tama) {
+int contarmatriz(int matrix, int rango) {
     int i, j;
     int large = 1;
-    if (tama <= 10) {
-        for (i = 0; i < tama; i++) {
-            for (j = 0; j < tama; j++) {
-                for (i == 1; i++ == 1; j++ == 1){
-                    do {
-                        large++;
-                    } while (i++ == 1 && j++ == 1);
-                }
+    for (i = 0; i < rango; i++) { 
+        for (j = 0; j < rango; j++) {
+            //con esto se recorre la matriz por fila y columna
+            for (i == 1; i++ == 1; j++ == 1){
+                //cuando llega a un 1, si el siguiente en fila y columna tambien son 1
+                do {
+                    large++;
+                    //suma uno al contador cada vez que se encuentra una posicion entre unos
+                } while (i++ == 1 && j++ == 1);
+                //cada vez que el numero en diagonal sea 1
             }
         }
-    } else {
-        for (i = 0; i < SIZE; i++) {
-            for (j = 0; j < SIZE; j++) {
-                for (i == 1; i++ == 1; j++ == 1){
-                    do {
-                        large++;
-                    } while (i++ == 1 && j++ == 1);
-                }
-            }
-        }
-
     }
 
     printf("El tamaño del cuadrado más grande de unos es: %dx%d\n", large, large);
@@ -72,9 +64,14 @@ int main() {
     printf("Ingrese el numero de filas de la matriz nxn:\n");
     scanf("%d", &tama);
 
-    int matrizfinal = crearmatriz(tama);
+    int matrizfinal = crearmatriz(tama); //asigna a una variable el valor de retorno (matriz en array) de la funcion
+    //para poder pasarlo como parametro a la siguiente funcion
 
-    contarmatriz(matrizfinal, tama);
+    if (tama <= 10) { //cumple con el tamano permitido
+        contarmatriz(matrizfinal, tama);
+    } else { //no cumple con el tamano y utiliza el macro que es el valor default
+        contarmatriz(matrizfinal, SIZE);
+    }
 
     return 0;
 }
